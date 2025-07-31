fdd5f221ba7fbfc07350d61e435d6ebfef06244e
Belén López <belop_18@hotmail.com>
2025-07-31 20:22:43 -0300

    v1.0.0.0 Primera entrega con Login y estructura inicial

---
+++ eProduccion/wwwroot/js/inicializadores.js
@@ -0,0 +1,8 @@
+﻿window.initializeSidebarToggle = function () {
+    setTimeout(function () {
+        $('.sidebar-toggler').off('click').on('click', function () {
+            $('.sidebar, .content').toggleClass("open");
+            return false;
+        });
+    }, 50); // Espera 50ms para asegurarse de que el DOM esté listo
+};
