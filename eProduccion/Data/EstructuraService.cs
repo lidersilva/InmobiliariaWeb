7aa629a734424075cfc7c9f50ec8755fa75806a2
Belén López <belop_18@hotmail.com>
2025-11-18 17:14:47 -0300

    Estructura Pendiente a molinar

--- eProduccion/Data/EstructuraService.cs
+++ eProduccion/Data/EstructuraService.cs
@@ -37,6 +37,7 @@ namespace eProduccion.Data
                 new () { TableName = "EEP_OT_ENSAM_CAB", Descr = "EEP OT Ensamblado cab.", ObjectType = 3 },
                 new () {TableName = "EEP_OT_ENSAM_DET", Descr = "EEP OT Ensamblado det.", ObjectType = 4},
                 new () {TableName = "EEP_REG_ENSAM_DET", Descr = "EEP Registro Ensamblado det.", ObjectType = 4},
+                new () { TableName = "EEP_PEND_MOLI_OT", Descr = "EEP OT Pendiente a molinar", ObjectType = 3 },
             }.ForEach(AgregarTablas);
             #endregion
 
@@ -240,6 +241,14 @@ namespace eProduccion.Data
                 new () { Name = "OPERARIO", Type = TipoCampo.Alpha, Size = 25, Description = "Operario", SubType = SubTipoCampo.None, TableName = "@EEP_REG_ENSAM_DET", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
                 new () { Name = "OPERARIO2", Type = TipoCampo.Alpha, Size = 25, Description = "Operario 2", SubType = SubTipoCampo.None, TableName = "@EEP_REG_ENSAM_DET", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
 
+                // OT Pendiente a molinar
+                new () { Name = "CODEPLANIOT", Type = TipoCampo.Numeric, Size = 11, Description = "Code planificación OT", SubType = SubTipoCampo.None, TableName = "@EEP_PEND_MOLI_OT", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
+                new () { Name = "ESTANTERIOR", Type = TipoCampo.Alpha, Size = 50, Description = "Estación anterior", SubType = SubTipoCampo.None, TableName = "@EEP_PEND_MOLI_OT", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
+                new () { Name = "OT", Type = TipoCampo.Numeric, Size = 11, Description = "Nro. OT", SubType = SubTipoCampo.None, TableName = "@EEP_PEND_MOLI_OT", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
+                new () { Name = "LINEIDOT", Type = TipoCampo.Numeric, Size = 11, Description = "LineId OT", SubType = SubTipoCampo.None, TableName = "@EEP_PEND_MOLI_OT", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
+                new () { Name = "LOTE", Type = TipoCampo.Alpha, Size = 25, Description = "Lote", SubType = SubTipoCampo.None, TableName = "@EEP_PEND_MOLI_OT", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
+                new () { Name = "CANTPROD", Type = TipoCampo.Numeric, Size = 11, Description = "Cantidad producida", SubType = SubTipoCampo.None, TableName = "@EEP_PEND_MOLI_OT", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
+                new () { Name = "CANTSOLICITADA", Type = TipoCampo.Numeric, Size = 11, Description = "Cantidad solicitada", SubType = SubTipoCampo.None, TableName = "@EEP_PEND_MOLI_OT", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
 
                 // Maestro paradas/motivos defectos
                 new () { Name = "CODIGO", Type = TipoCampo.Alpha, Size = 3, Description = "Código mot. parada/defecto", SubType = SubTipoCampo.None, TableName = "@EEP_PARADA_DEFECTO", LinkedTable = null, DefaultValue = null, ValidValuesMD = valoresValidosVacios, LinkedSystemObject = null },
