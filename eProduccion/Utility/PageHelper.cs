d2889eac97a0b8a8096c64cf6d61f1e5e79e77c0
lidersilva <lider.silva@exxis-group.com>
2025-12-15 11:19:25 -0300

    Taks: Ajuste verificar

--- eProduccion/Utility/PageHelper.cs
+++ eProduccion/Utility/PageHelper.cs
@@ -4,15 +4,15 @@ namespace eProduccion.Utility
 {
     public static class PageHelper
     {
-        public static Color DefinirColorEstado(string estado)
+        public static MudBlazor.Color DefinirColorEstado(string estado)
         {
             return estado switch
             {
-                "Pendiente" => Color.Default,
-                "En proceso" => Color.Info,
-                "Finalizado" => Color.Success,
-                "Anulado" => Color.Dark,
-                _ => Color.Transparent
+                "Pendiente" => MudBlazor.Color.Default,
+                "En proceso" => MudBlazor.Color.Info,
+                "Finalizado" => MudBlazor.Color.Success,
+                "Anulado" => MudBlazor.Color.Dark,
+                _ => MudBlazor.Color.Transparent
             };
         }
     }
